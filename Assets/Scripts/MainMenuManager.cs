62716613173f937217fe658ba95dbbb8cc450183
nicorm2000 <nicorm2000@gmail.com>
2023-10-11 23:51:38 -0300

    Finished the Main Menu Manager

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
+using System.Collections;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private TMP_Text _newBestText;
+    [SerializeField] private TMP_Text _highScoreText;
+    [SerializeField] private AudioClip _clickClip;
+    [SerializeField] private float _animationTime;
+    [SerializeField] private AnimationCurve _speedCurve;
+
+    private void Awake()
     {
-        
+        if (GameManager.instance.isInitialized)
+        {
+            StartCoroutine(ShowScore());
+        }
+        else
+        {
+            _scoreText.gameObject.SetActive(false);
+            _newBestText.gameObject.SetActive(false);
+            _highScoreText.text = GameManager.instance.highScore.ToString();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ShowScore()
     {
-        
+        int tempScore = 0;
+        _scoreText.text = tempScore.ToString();
+
+        int currentScore = GameManager.instance.currentScore;
+        int highScore = GameManager.instance.highScore;
+
+        if (highScore < currentScore)
+        {
+            _newBestText.gameObject.SetActive(true);
+            GameManager.instance.highScore = currentScore;
+        }
+        else
+        {
+            _newBestText.gameObject.SetActive(false);
+        }
+
+        _highScoreText.text = GameManager.instance.highScore.ToString();
+
+        float speed = 1 / _animationTime;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < _animationTime)
+        {
+            timeElapsed += speed * Time.deltaTime;
+
+            tempScore = (int)(_speedCurve.Evaluate(timeElapsed) * currentScore);
+            _scoreText.text = tempScore.ToString();
+            
+            yield return null;
+        }
+
+        tempScore = currentScore;
+        _scoreText.text = tempScore.ToString();
+    }
+
+    public void ClickedPlay()
+    {
+        AudioManager.instance.PlaySound(_clickClip);
+        GameManager.instance.GoToGame();
     }
 }
