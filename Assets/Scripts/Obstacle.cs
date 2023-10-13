3812932c59542651582527d928ec9e89139f107e
nicorm2000 <nicorm2000@gmail.com>
2023-10-13 11:24:15 -0300

    Fixed some issues that weren't saved, working on the obstacle script

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float minRotateSpeed, maxRotateSpeed;
+    [SerializeField] private float minRotateTime, maxRotateTime;
+
+    private bool _hasGameFinished;
+    private float _currentRotateSpeed;
+    private float _currentRotateTime;
+    private float _rotateTime;
+
+    private void Awake()
     {
-        
+        _hasGameFinished = false;
+        _currentRotateTime = 0;
+        _currentRotateSpeed = minRotateSpeed + (maxRotateSpeed - minRotateSpeed) * Random.Range(0,11) * 0.1f;
+        _rotateTime = minRotateTime + (maxRotateTime - minRotateTime) * Random.Range(0, 11) * 0.1f;
+        _currentRotateSpeed *= Random.Range(0,2) == 0 ? 1f : -1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _currentRotateTime += _currentRotateSpeed * Time.deltaTime;
+        if (_currentRotateTime > _rotateTime)
+        {
+            _currentRotateTime = 0;
+            _currentRotateSpeed = minRotateSpeed + (maxRotateSpeed - minRotateSpeed) * Random.Range(0, 11) * 0.1f;
+            _rotateTime = minRotateTime + (maxRotateTime - minRotateTime) * Random.Range(0, 11) * 0.1f;
+            _currentRotateSpeed *= Random.Range(0, 2) == 0 ? 1f : -1f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_hasGameFinished)
+        {
+            return;
+        }
+        transform.Rotate(0, 0, _currentRotateSpeed * Time.fixedDeltaTime);
     }
 }
