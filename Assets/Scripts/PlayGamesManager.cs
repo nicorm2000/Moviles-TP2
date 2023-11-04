aa9a394796421c058b0db115d6fc05ad3dc7110b
nicorm2000 <nicorm2000@gmail.com>
2023-11-04 18:46:23 -0300

    Create a login manager with the google play services and a scene that would take me to the game

---
+++ Assets/Scripts/PlayGamesManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine.SceneManagement;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using UnityEngine;
+using TMPro;
+
+public class PlayGamesManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI detailsText;
+
+    private void Start()
+    {
+        SignIn();
+    }
+
+    public void SignIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
+            string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+            string id = PlayGamesPlatform.Instance.GetUserId();
+            string imgURL = PlayGamesPlatform.Instance.GetUserImageUrl();
+
+            detailsText.text = "Success \n" + name;
+
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            detailsText.text = "Sign In failed!";
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        }
+    }
+
+}
