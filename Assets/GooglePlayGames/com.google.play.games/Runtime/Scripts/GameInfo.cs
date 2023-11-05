8cacacc71b6c0dd9273c2601bd88e7241483e98d
nicorm2000 <nicorm2000@gmail.com>
2023-11-05 17:36:09 -0300

    Setted up Google Play services

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,10 +36,10 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "425860275805"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
-        public const string NearbyConnectionServiceId = "";
+        public const string WebClientId = "425860275805-05cgt5d2r4d0kcgs65mco70tr6uuqrkr.apps.googleusercontent.com"; // Filled in automatically
+        public const string NearbyConnectionServiceId = "com.RamosMarin2023.DOT";
 
         public static bool ApplicationIdInitialized() {
             return !string.IsNullOrEmpty(ApplicationId) && !ApplicationId.Equals(ToEscapedToken(UnescapedApplicationId));
