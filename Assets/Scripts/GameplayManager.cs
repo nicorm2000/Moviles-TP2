223c30ca6e8cb496afc04af59449379cd7dae475
nicorm2000 <nicorm2000@gmail.com>
2023-11-16 16:16:50 -0300

    Added new mini tutorial

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -8,8 +8,11 @@ public class GameplayManager : MonoBehaviour
 {
     [SerializeField] private GameObject tutorialIcon;
     [SerializeField] private GameObject tutorialText;
+    [SerializeField] private GameObject tutorialSemiCircle;
     [SerializeField] private TMP_Text score_Text;
     [SerializeField] private List<int> levelSpeed, levelMax;
+    [SerializeField] private float tutorialSemiCircleTime;
+    [SerializeField] private float gameOverTime;
 
     public bool hasStarted = false;
 
@@ -39,6 +42,7 @@ public class GameplayManager : MonoBehaviour
         if (hasStarted)
         {
             HideTutorial();
+            StartCoroutine(HideTutorialSemiCircle());
             _score += _scoreSpeed * Time.deltaTime;
             score_Text.text = ((int)_score).ToString();
 
@@ -68,5 +72,29 @@ public class GameplayManager : MonoBehaviour
     {
         tutorialIcon.SetActive(false);
         tutorialText.SetActive(false);
+    }
+
+    private IEnumerator HideTutorialSemiCircle()
+    {
+        TextMeshProUGUI textMeshPro = tutorialSemiCircle.GetComponent<TextMeshProUGUI>();
+
+        float elapsedTime = 0f;
+
+        Color textColor = textMeshPro.color;
+
+        while (elapsedTime < tutorialSemiCircleTime)
+        {
+            float remainingTime = tutorialSemiCircleTime - elapsedTime;
+            float alpha = remainingTime / tutorialSemiCircleTime;
+
+            textColor.a = alpha;
+            textMeshPro.color = textColor;
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        tutorialSemiCircle.SetActive(false);
     }
 }
