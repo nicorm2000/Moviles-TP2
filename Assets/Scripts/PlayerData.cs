bdf50f0076fb3b4c06e38bcd818e16ef2e8eaf8c
nicorm2000 <nicorm2000@gmail.com>
2023-11-24 14:00:18 -0300

    Deleted buggy animation, added rodri to credits and created scriptable object to handle players skin

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
+public class PlayerData : ScriptableObject
+{
+    public SkinVariant[] skinVariants;
+    public int equippedSkinIndex;
+}
+
+[System.Serializable]
+public class SkinVariant
+{
+    public string skinName;
+    public Sprite skinSprite;
+    public int cost;
+}
